8054011b5acadc13bbdacc0a1e3299645f06aabd
johndembaremba <59832682+John-Dembaremba@users.noreply.github.com>
2022-03-21 12:30:43 +0200

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
-from betterforms.multiform import MultiModelForm
 from system.models import Profile
 
 class SignUpForm(UserCreationForm):
